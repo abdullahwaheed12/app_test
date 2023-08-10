bc8bbab1286e7021b7abba6183a2e6a8a580afaa
abdullahwaheed <pgcbwp12@gmail.com>
2023-08-10 22:50:58 +0500

    udpate

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -6,6 +6,6 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const ScaffoldWidget(child: SizedBox());
+    return const ScaffoldWidget(useAppbar: false, child: SizedBox());
   }
 }
